0acf5f755f7842f35d97a8ab5d1acb158645e07e
Rokr0k <rokr0k@rokr0k.xyz>
2025-04-06 17:25:03 +0900

    week2 with image display

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,42 @@
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *window = SDL_CreateWindow("Nanbou", 1280, 720, 0);
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
+    SDL_CreateWindowAndRenderer("Nanbou", 1280, 720, 0, &window, &renderer);
+    SDL_SetRenderVSync(renderer, 1);
 
-    SDL_Delay(5000);
+    SDL_Surface *surface = SDL_LoadBMP("res/redbull.bmp");
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_DestroySurface(surface);
 
+    bool quit = false;
+    while (!quit)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                quit = true;
+            }
+        }
+
+        SDL_SetRenderDrawColorFloat(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(texture);
+
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
     SDL_Quit();
